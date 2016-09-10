d8b3c98ba31c04cbc88ed01da5ad223f00ae4be3
xtofl <kristoffel.pirard@gmail.com>
2016-09-10 17:25:27 +0200

    test: new round should be added

--- tests/test_pairings.py
+++ tests/test_pairings.py
@@ -1,8 +1,8 @@
 import pairings
 
 def test_ordered_by_player():
-    history = {
-        1: {'pairing': [], 'result': []}
-    }
-    pairings.by_player(status)
-    assert 1 == 2
+    history = [
+        {'pairing': [], 'result': []}
+    ]
+    new = pairings.by_player(history)
+    assert new[1] != None
